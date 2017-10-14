f6fa544c9afe07209f550f853ddae3ec6648a20d
gueux <gueux13@gmail.com>
2017-10-14 09:13:43 -0400

    akeFiles sR

--- sR.bash
+++ sR.bash
@@ -1,6 +1,14 @@
 #!/bin/bash
 n128=0
 theme='massive-spike'
+lib='mpiTest-massive-NEURON.mat'
+rE=40
+rI=60
+t=1000
+v0=1
+tref=13
+seed=193864
+
 dir0='singleRun'
 dir1=$dir0'/'$theme
 if [ -d "$dir1" ]; then
@@ -15,9 +23,10 @@ else
     cp base/n128.py_active $dir1/n128.py
 fi
 cp base/neuroAlter.py $dir1
-cp $dir0/sr.slurm $dir1
+cp $dir0/sR.slurm $dir1
 cp -R x86_64 $dir1
 
 cd $dir1
 
-sbatch --export=theme=$theme sr.slurm
+sbatch --export=theme=$theme,v0=$v0,rE=$rE,rI=$rI,t=$t,tref=$tref,seed=$seed,lib=$lib sR.slurm
+
