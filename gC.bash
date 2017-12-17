3ffae990a1f89d1f1cf60d5fe9018596d379dc81
wd554 <wd554@nyu.edu>
2017-12-17 20:48:50 +0400

    plot

--- gC.bash
+++ gC.bash
@@ -27,6 +27,7 @@ cp $dir0/gC.slurm $dir
 cp gainCurve.cfg $dir/input.cfg
 cp $dir0/gainCurve $dir
 cp $dir0/plotGainCurve.m $dir
+cp $dir0/read_cfg.m $dir
 cp -R x86_64 $dir
 
 cp $dir0/levels.bin $dir
